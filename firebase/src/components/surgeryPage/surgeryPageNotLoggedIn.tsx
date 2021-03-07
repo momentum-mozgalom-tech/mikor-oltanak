880cfcc37ddca8dd60546ae660a454f7dd5e3d83
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2021-03-07 20:09:49 +0100

    Fix incorrect grid usage

--- firebase/src/components/surgeryPage/surgeryPageNotLoggedIn.tsx
+++ firebase/src/components/surgeryPage/surgeryPageNotLoggedIn.tsx
@@ -12,7 +12,7 @@ export function SurgeryPageNotLoggedIn() {
             <Grid item xs={12} md={6}>
                 <Typography variant="h2" gutterBottom>Háziorvosoknak/rendelőknek</Typography>
             </Grid>
-            <Grid item xs={12} md={6} direction="column" alignItems="stretch">
+            <Grid item xs={12} md={6} container direction="column" alignItems="stretch">
                 <Typography variant="body1" paragraph>
                     Háziorvosok/rendelők ezen a lapon állíthatják be,
                     hogy mely páciensek szerepelnek az oltási listájukon.
@@ -24,18 +24,18 @@ export function SurgeryPageNotLoggedIn() {
             <Grid item xs={12} md={6} container direction="column" alignItems="stretch">
                 <Login redirectUrl={NavUtils.getNavUrl[Page.Rendelo]()} />
             </Grid>
-            <Grid item xs={12} md={6} direction="column" alignItems="stretch">
+            <Grid item xs={12} md={6} container direction="column" alignItems="stretch">
                 <Typography variant="body1" paragraph>
                     Ha még nem regisztrált, akkor azt az alábbi gombra kattintva teheti meg.
                     Igyekszünk minél hamarabb válaszolni!
                 </Typography>
             </Grid>
-            <Grid item xs={12} md={6} direction="column" alignItems="stretch">
+            <Grid item xs={12} md={6} container direction="column" alignItems="stretch">
                 <Button color="primary" variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLSeL0p2n5BH5xMGdPTLSlhFG1Q3uF4ewiKCUGUzV1-80FDu41A/viewform?usp=sf_link" target="_blank" rel="noreferrer">
                     Regisztrálok háziorvosként/rendelőként
                 </Button>
             </Grid>
-            <Grid item xs={12} md={6} direction="column" alignItems="stretch">
+            <Grid item xs={12} md={6} container direction="column" alignItems="stretch">
                 <Typography variant="body1" paragraph>
                     Kérdéseit küldje a ????@mikoroltanak.eu címre.
                 </Typography>
