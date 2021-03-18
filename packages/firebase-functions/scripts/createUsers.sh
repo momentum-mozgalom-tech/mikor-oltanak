9cbf9803de8ca6697a2568925715e37e86f76e36
Miklos Danka <8069999+mdanka@users.noreply.github.com>
2021-03-18 21:09:51 +0100

    Add instructions to the user creation script

--- packages/firebase-functions/scripts/createUsers.sh
+++ packages/firebase-functions/scripts/createUsers.sh
@@ -5,5 +5,9 @@ set -eux
 # 2. Update createUsers.ts to reference this new file
 # 3. Set the following env var:
 #    export GOOGLE_APPLICATION_CREDENTIALS="/path/to/secret/mikor-oltanak-firebase-adminsdk.json"
+# 4. Copy the applicants into secret/new-users.txt in the following format:
+#    email <tab> name <tab> description
+#    email <tab> name <tab> description
+#    email <tab> name <tab> description
 
 pnpx ts-node src/createUsers.ts
