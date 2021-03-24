f38fedbc7b5121046f25cf2acffc9df85b665bcf
Miklos Danka <danka.miklos@gmail.com>
2021-03-24 19:08:09 +0100

    Secrets folder instead of secret

--- packages/firebase-functions/scripts/createUsers.sh
+++ packages/firebase-functions/scripts/createUsers.sh
@@ -1,10 +1,10 @@
 set -eux
 
-# 1. Get a private key for the firebase service account and place it in the /secret folder:
+# 1. Get a private key for the firebase service account and place it in the /secrets folder:
 #    https://console.firebase.google.com/u/0/project/mikor-oltanak/settings/serviceaccounts/adminsdk
 # 2. Update createUsers.ts to reference this new file
 # 3. Set the following env var:
-#    export GOOGLE_APPLICATION_CREDENTIALS="/path/to/secret/mikor-oltanak-firebase-adminsdk.json"
+#    export GOOGLE_APPLICATION_CREDENTIALS="/path/to/secrets/mikor-oltanak-firebase-adminsdk.json"
 # 4. Copy the applicants into secret/new-users.txt in the following format:
 #    email <tab> name <tab> description
 #    email <tab> name <tab> description
